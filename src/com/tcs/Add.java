d8c21a4beac490105cfa7d74147acc66c72db5f2
FarukAli <ansarifaruk8454@gmail.com>
2023-05-31 14:59:32 +0530

    addtion

---
+++ src/com/tcs/Add.java
@@ -0,0 +1,12 @@
+package com.tcs;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=20;
+		int result;
+		result=num1+num2;
+		System.out.println("Addition of two number "+result);
+	}
+}
