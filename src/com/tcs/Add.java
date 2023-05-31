fd342da324675490d8d9d30af0a50d4e08df129f
FarukAli <ansarifaruk8454@gmail.com>
2023-05-31 15:24:19 +0530

    changes another function add

--- src/com/tcs/Add.java
+++ src/com/tcs/Add.java
@@ -2,11 +2,22 @@ package com.tcs;
 
 public class Add {
 
+	public int sub(int a, int b)
+	{
+		int c = a-b;
+		System.out.println(c);
+		return c;
+	}
 	public static void main(String[] args) {
 		int num1=10;
 		int num2=20;
 		int result;
 		result=num1+num2;
 		System.out.println("Addition of two number "+result);
+		
+		Add s= new Add();
+		s.sub(5,3);
 	}
+	
+	
 }
